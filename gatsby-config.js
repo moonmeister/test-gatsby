d7516b55728d9d533f77a55029d42d997a5ba149
Alex Moon <alex.jared.moon@gmail.com>
2019-03-26 04:44:06 +0000

    set env versions

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `jacob-herper`,
+        short_name: `jacob`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#E94E1B`,
+        theme_color: `#E94E1B`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
